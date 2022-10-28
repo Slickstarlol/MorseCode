02c475013a66f2c6f22fcb8d592c59d21990b25e
PC <PC@DESKTOP-0BHVMSN>
2022-10-28 02:38:00 -0700

    Added PIE graphs

--- morse code/Main.m
+++ morse code/Main.m
@@ -1,3 +1,13 @@
 
-stuff = morseCreation("Hello");
-morseDecryption(stuff)
+%stuff = morseCreation("...././.-../.-../---/",false)
+%pie(categorical(num2cell(stuff)))
+
+%charCell = unique(stuff)
+%n = histcounts(stuff,charCell)
+
+stuff = morseCreation("-.-./---/...-/../-../.----/----./..--.-/.--./.-/-./-.././--/../-.-./..--.-/...-/.-/-.-./-.-./../-././",false)
+B = char(num2cell(stuff));
+[A,C] = groupcounts(B);
+
+%pie(categorical(num2cell(stuff)))
+pie3(A, C +": "+A+" times")
