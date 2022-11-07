3829a735652f848881754eb1c6b73508dfb64c83
PC <PC@DESKTOP-0BHVMSN>
2022-11-07 10:29:18 -0800

    Added more comments

--- morse code - Kentstar Harsono/KentstarHarsono.m
+++ morse code - Kentstar Harsono/KentstarHarsono.m
@@ -7,7 +7,7 @@ while true
     if (all(isstrprop(keyWord, 'punct')==1)) % If punctuations included in input (morse or random symbol)
         try % If morse code
             stuff = morseCreation(keyWord,false) % Decyphers code
-            wordsInputed = [cell(wordsInputed),stuff];
+            wordsInputed = [cell(wordsInputed),stuff]; % Allocates stuff into wordsInputed
             for i=1:length(wordsInputed)
                 subplot(1,length(wordsInputed),i) %Allocates wordsInputed into subplots
                 pieGraph(wordsInputed{i}); % Creates pie graph
