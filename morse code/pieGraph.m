946e6464ce7b036411825cc298eeab6c26931bc4
PC <PC@DESKTOP-0BHVMSN>
2022-11-06 22:04:31 -0800

    Fixed bug

--- morse code/pieGraph.m
+++ morse code/pieGraph.m
@@ -2,7 +2,7 @@ function pieGraph(text)
     B = char(num2cell(text)); % Converts str to cell array
     [totalEachNumber,Characters] = groupcounts(B); % Checks [totalEachNumber] in each letter [Characters] in B
     pie3(totalEachNumber, logical(totalEachNumber == max(totalEachNumber)).'... % Explodes using logic array for max(char) in text
-        ,Characters +": "+totalEachNumber+" times") % Creates 3D pie graph
+        ,num2cell(Characters +": "+int2str(totalEachNumber)+" times")) % Creates 3D pie graph
     title({"Characters used in the word:",text}) %Initializing pie title
 end
 
